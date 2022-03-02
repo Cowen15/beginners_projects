76e366f03f058512aabe5631fead7349a999deb2
Crystal Owen <87962304+Cowen15@users.noreply.github.com>
2022-03-02 15:31:47 -0600

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,35 @@
+import random
+
+top_range = input('Type a number: ')
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0:
+        print('Please type a number larger than 0 next time.')
+        quit()
+else:
+    print('Please type a number next time.')
+    quit()
+
+random_number = random.randint(0, top_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input('Please guess a number: ')
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print('You got it!')
+        break
+    elif user_guess < random_number:
+        print('Too low, try again!')
+    else:
+        print('Too high, try again!')
+
+print(f'You got it in {guesses} guesses.')
